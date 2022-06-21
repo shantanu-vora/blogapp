64fcaa4ec6bf18a25be2c7bf2282a9b860fec975
Shantanu Vora <shantu.git@gmail.com>
2022-06-22 02:07:38 +0530

    Add Tag Entity class in com.shantanu.blogapp.entity package

---
+++ src/main/java/com/shantanu/blogapp/entity/Tag.java
@@ -0,0 +1,55 @@
+package com.shantanu.blogapp.entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="tag")
+public class Tag {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "created_at")
+	private Timestamp createdAt;
+
+	@Column(name = "updated_at")
+	private Timestamp updatedAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
