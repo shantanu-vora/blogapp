9ff76e9a8303071ac129ad0f8af45f8e146cd80c
Shantanu Vora <shantu.git@gmail.com>
2022-06-22 02:14:07 +0530

    Add Interface TagRepository that extends JpaRepository

---
+++ src/main/java/com/shantanu/blogapp/repository/TagRepository.java
@@ -0,0 +1,10 @@
+package com.shantanu.blogapp.repository;
+
+import com.shantanu.blogapp.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+
+}
