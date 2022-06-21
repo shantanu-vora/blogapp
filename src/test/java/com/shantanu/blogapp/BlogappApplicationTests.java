9cf59c9740853f214d259db4af615166e1f55dc2
Shantanu Vora <shantu.git@gmail.com>
2022-06-21 12:01:04 +0530

    Initial Commit

---
+++ src/test/java/com/shantanu/blogapp/BlogappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shantanu.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
