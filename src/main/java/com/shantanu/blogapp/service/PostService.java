d133f93450170ae9a7d427a4040975d5d260d835
Shantanu Vora <shantu.git@gmail.com>
2022-06-22 17:31:14 +0530

    Add getAllPost() method in the PostService interface

--- src/main/java/com/shantanu/blogapp/service/PostService.java
+++ src/main/java/com/shantanu/blogapp/service/PostService.java
@@ -2,6 +2,9 @@ package com.shantanu.blogapp.service;
 
 import com.shantanu.blogapp.entity.Post;
 
+import java.util.List;
+
 public interface PostService {
 	void savePost(Post post);
+	List<Post> getAllPosts();
 }
