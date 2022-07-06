582ee79fe9909dfad23150673544d8c1d5761624
Shantanu Vora <shantu.git@gmail.com>
2022-07-06 23:05:32 +0530

    Refactor | Replace depracated  WebSecurityConfigurerAdapter with SecurityFilterChain

--- src/main/java/com/shantanu/blogapp/config/SecurityConfiguration.java
+++ src/main/java/com/shantanu/blogapp/config/SecurityConfiguration.java
@@ -3,37 +3,31 @@ package com.shantanu.blogapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import java.security.SecureRandom;
 
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
 	@Autowired
 	UserDetailsService userDetailsService;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-						.antMatchers( "/post/newPost", "/post/edit/*", "/post/drafts/**").authenticated()
+						.antMatchers("/post/newPost", "/post/edit/*", "/post/drafts/**").authenticated()
 						.antMatchers(HttpMethod.POST, "/post/delete/{id}").authenticated()
 						.antMatchers("/**").permitAll()
-						.and().exceptionHandling().accessDeniedPage("/accessDenied")
 						.and()
 						.formLogin().loginPage("/login").defaultSuccessUrl("/")
 						.and().logout()
 						.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/");
+						return http.build();
 	}
 
 	@Bean
