dab731805b32743e619e181dc6efc017ebadb7d4
Shantanu Vora <shantu.git@gmail.com>
2022-07-09 15:32:31 +0530

    Refactor | Add Swagger dependency and antmatchers

--- src/main/java/com/shantanu/blogapp/config/SecurityConfiguration.java
+++ src/main/java/com/shantanu/blogapp/config/SecurityConfiguration.java
@@ -9,9 +9,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.security.SecureRandom;
 
+@EnableWebMvc
 @EnableWebSecurity
 public class SecurityConfiguration {
 
@@ -26,6 +28,7 @@ public class SecurityConfiguration {
 						.antMatchers(HttpMethod.PUT, "/api/posts/{id}", "/api/posts/{id}/comments/{commentId}").authenticated()
 						.antMatchers(HttpMethod.DELETE, "/api/posts/{id}", "/api/posts/{postId}/comments/{commentId}").authenticated()
 						.antMatchers("/post/newPost", "/post/edit/*", "/post/drafts/**").authenticated()
+						.antMatchers("/v3/api-docs","/v2/api-docs","/swagger-resources/","/swagger-ui/","/webjars/","/api/v1/auth/"). permitAll()qqq
 						.antMatchers("/**").permitAll()
 						.anyRequest().authenticated()
 						.and()
